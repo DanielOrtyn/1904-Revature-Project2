ee3e6befc3dbbd44f6d8629733934dc733288949
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-05-15 09:56:55 -0400

    weird connect error fixed

--- src/components/sign-in/sign-out.component.tsx
+++ src/components/sign-in/sign-out.component.tsx
@@ -1,36 +1,46 @@
-import { RouteComponentProps } from "react-router";
-import { IUserState, IState } from "../../reducers";
-import React from "react";
-import { login } from "../../actions/user.actions";
-import { connect } from "http2";
+import React from 'react';
+import { IUserState, IState } from '../../reducers';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { login } from '../../actions/user.actions';
 
-interface ISignInState {
-    username: string;
-    password: string;
+interface ISignOutState {
+  username: string;
+  password: string;
 }
 
-interface ISignInProps extends RouteComponentProps<{}>{
-    auth: IUserState
-    login: (username: string, password: string, history: any) => void
+interface ISignOutProps extends RouteComponentProps<{}>{
+  auth: IUserState
+  login: (username: string, password: string, history: any) => void
 }
-  
-export class SignOutComponent extends React.Component<ISignInProps, ISignInState> {
-    render(){
-        return(
-            <div>
-                
-            </div>
-        )
-    }   
+
+export class SignOutComponent extends React.Component<ISignOutProps, ISignOutState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  render() {
+    const { username, password } = this.state;
+    const errorMessage = this.props.auth.errorMessage;
+    return (
+      <div>
+      </div>
+    );
+  }
 }
+
 const mapStateToProps = (state: IState) => {
-    return  {
-      auth: state.auth
-    }
+  return  {
+    auth: state.auth
   }
-  
-  const mapDispatchToProps = {
-    login: login
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(SignOutComponent);
+}
+
+const mapDispatchToProps = {
+  login: login
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignOutComponent);
