5380527283c66fff8fb2c1d303677b7e747ffb2b
Sperj001 <josephsperduto@gmail.com>
2019-05-16 16:38:18 -0400

    New css

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -14,12 +14,22 @@ export class NavComponent extends React.PureComponent<INaveProps> {
         const currentUser = this.props.currentUser;
         return (
             <nav className="navbar navbar-toggleable-md navbar-expand-lg navbar-dark bg-dark display-front nav-pad">
+                
+                <table>
+                    <tbody className="centered">
+                        <tr><td>
+                            <div className="userDisplay">{currentUser && "Welcome"}</div>
+                        </td></tr>
+                        <tr><td>
+                            <div className="userDisplay">{currentUser && <Link to="/user-info" className="unset-anchor userLink">{currentUser.name}</Link>}</div>
+                        </td></tr>
+                    </tbody>
+                </table>
                 <div className="navbar-header c-pointer shift-left">
                     <Link to="/home" className="unset-anchor">
                         <img className="img-adjust-position rev-logo" src={RevLogo} alt="revature" />
                     </Link>
                 </div>
-                <div>{currentUser && currentUser.name}</div>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -34,7 +44,7 @@ export class NavComponent extends React.PureComponent<INaveProps> {
 
                                 ? <li className="nav-item active dropdown">
 
-                                <div className="nav-link dropdown-toggle pointer" id="examples-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User Options</div>
+                                <div className="nav-link dropdown-toggle pointer" id="examples-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >User Options</div>
                                 <div className="dropdown-menu" aria-labelledby="examples-dropdown">
                                     <div className="dropdown-item"><Link to="/user-info" className="unset-anchor nav-link">View Myself</Link></div>
                                     <div className="dropdown-item"><Link to="/sign-out" className="unset-anchor nav-link">Sign Out</Link></div>
