08eda6cdc119569f53bcd6c31391cf68d244ff98
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-05-16 21:33:16 -0400

    Make update bid option implemented

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,36 @@ import { HomeComponent } from './components/home/home.component';
 import SignInComponent from './components/sign-in/sign-in.component';
 import { Provider } from 'react-redux';
 import { store } from './Store';
-import  UserInfoComponent  from './components/user/user.info.page.component';
-import  SaleItemComponent  from './components/saleitem/saleitem.component';
+import UserInfoComponent from './components/user/user.info.page.component';
+import SaleItemComponent from './components/saleitem/saleitem.component';
 import { SaleItemCategoryListComponent } from './components/SaleItemList/saleitem-category-list.component';
 import { CreateUserInfoComponent } from './components/user/user.create.page.component';
 import SignOutComponent from './components/sign-in/sign-out.component';
 import EditUserInfoComponent from './components/user/user.edit.page.component';
 
 const App: React.FC = () => {
-  return (
-    <body className="MainBackground">
-      <Provider store={store}>
-        <BrowserRouter>
-          <NavComponent />
-          <div id="main-content-container">
-            <Switch>>
-              <Route path="/home" component={HomeComponent} />
-              <Route path="/sign-in" component={SignInComponent} />
-              <Route path="/user-info" component={UserInfoComponent}/>
-              <Route path="/saleitemList" component={SaleItemCategoryListComponent}/>
-              <Route path="/create-user" component={CreateUserInfoComponent}/>
-              <Route path="/sign-out" component={SignOutComponent}/>
-              <Route path="/sale-page" component={SaleItemComponent}/>
-              <Route path="/edit-user" component={EditUserInfoComponent}/>
-              <Route component={HomeComponent} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </Provider>
-    </body>
-  );
+    return (
+        <body className="MainBackground">
+            <Provider store={store}>
+                <BrowserRouter>
+                    <NavComponent />
+                    <div id="main-content-container">
+                        <Switch>
+                            <Route path="/home" component={HomeComponent} />
+                            <Route path="/sign-in" component={SignInComponent} />
+                            <Route path="/user-info" component={UserInfoComponent} />
+                            <Route path="/saleitemList" component={SaleItemCategoryListComponent} />
+                            <Route path="/create-user" component={CreateUserInfoComponent} />
+                            <Route path="/sign-out" component={SignOutComponent} />
+                            <Route path="/sale-page" component={SaleItemComponent} />
+                            <Route path="/edit-user" component={EditUserInfoComponent} />
+                            <Route component={HomeComponent} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </Provider>
+        </body>
+    );
 }
 
 export default App;
--- src/components/saleitem/saleitem.component.tsx
+++ src/components/saleitem/saleitem.component.tsx
@@ -2,23 +2,50 @@ import React from 'react';
 import { SaleItem } from '../../model/saleItem';
 import { IState } from '../../reducers';
 import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { NewBidCardComponent } from '../bids/bid.create.profile.card.somponent';
+import { User } from '../../model/user';
 
 
-interface ISaleItemComponentProps {
-    item?: SaleItem;
+interface ISaleItemComponentProps extends RouteComponentProps<{}> {
+    currentUser: User;
+    item: SaleItem;
 }
 
-export class SaleItemComponent extends React.PureComponent<ISaleItemComponentProps> {
-    constructor(props){
+interface ISaleItemComponentHistory {
+    allowBid: boolean;
+}
+
+export class SaleItemComponent extends React.Component<ISaleItemComponentProps, ISaleItemComponentHistory> {
+    constructor(props) {
         super(props);
+        this.state = {
+            allowBid: false
+        }
     }
     openBidMaker = () => {
-        // send user to a make bid page
-        throw ('Code not implemented. Make Daniel do his job better');
+        if (this.props.currentUser) {
+            this.setState({
+                allowBid: true
+            })
+        } else {
+            this.props.history.push('/sign-in');
+        }
     }
 
     renderItemLoadFailedComponent() {
         return (<p>Warning, Item Did Not Load</p>);
+    }
+
+    renderBid() {
+        if (this.state.allowBid) {
+            return (
+                <NewBidCardComponent saleItem={this.props.item} currentUser={this.props.currentUser}
+                    history={this.props.history} location={this.props.location}
+                    match={this.props.match} />
+            );
+        }
+        return <></>;
     }
 
     renderItemComponent() {
@@ -26,14 +53,19 @@ export class SaleItemComponent extends React.PureComponent<ISaleItemComponentPro
             return (
                 <>
                     <div>
-                        <img src={this.props.item.itemImg.url} alt='test' width='300' height='200' />
-                    </div>
-                    <div className='bidDetailDiv'>
-                        <h1>{this.props.item.title}</h1>
-                        <p>Price: ${this.props.item.currentBid.currentBidPrice}</p>
-                        <h4>Description</h4>
-                        <div>{this.props.item.description}</div>
-                        <button className="btn btn-primary" onClick={this.openBidMaker}>Place Bid</button>
+                        <div>
+                            <img src={this.props.item.itemImg.url} alt='test' width='300' height='200' />
+                        </div>
+                        <div className='bidDetailDiv'>
+                            <h1>{this.props.item.title}</h1>
+                            <p>Price: ${this.props.item.currentBid.currentBidPrice}</p>
+                            <h4>Description</h4>
+                            <div>{this.props.item.description}</div>
+                            <button className="btn btn-primary" onClick={this.openBidMaker}>Place Bid</button>
+                        </div>
+                        <div>
+                            {this.renderBid()}
+                        </div>
                     </div>
                 </>
             );
@@ -54,6 +86,7 @@ export class SaleItemComponent extends React.PureComponent<ISaleItemComponentPro
 }
 const mapStateToProps = (state: IState) => {
     return {
+        currentUser: state.auth.currentUser,
         item: state.sales.displayItem
     }
 }
