68da9abbfa80e5a50f043c667c49b48a301b8f2d
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-05-17 14:33:23 -0400

    tetasdfdsgsd

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,10 @@ import { Provider } from 'react-redux';
 import { store } from './Store';
 import UserInfoComponent from './components/user/user.info.page.component';
 import SaleItemComponent from './components/saleitem/saleitem.component';
-import { SaleItemCategoryListComponent } from './components/SaleItemList/saleitem-category-list.component';
 import { CreateUserInfoComponent } from './components/user/user.create.page.component';
 import SignOutComponent from './components/sign-in/sign-out.component';
 import EditUserInfoComponent from './components/user/user.edit.page.component';
-import NewSaleCardComponent from './components/saleitem/saleitem.new.card.somponent';
+import { SaleItemCategoryListComponent } from './components/SaleItemList/saleitem-category-list.component';
 
 const App: React.FC = () => {
   return (
@@ -26,7 +25,7 @@ const App: React.FC = () => {
               <Route path="/home" component={HomeComponent} />
               <Route path="/sign-in" component={SignInComponent} />
               <Route path="/user-info" component={UserInfoComponent}/>
-              <Route path="/saleitemList" component={SaleItemCategoryListComponent}/>
+              <Route path="/saleitem-List" component={SaleItemCategoryListComponent}/>
               <Route path="/create-user" component={CreateUserInfoComponent}/>
               <Route path="/sign-out" component={SignOutComponent}/>
               <Route path="/sale-page" component={SaleItemComponent}/>
--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -58,9 +58,9 @@ export class NavComponent extends React.PureComponent<INaveProps> {
                         <li className="nav-item active">
                             <Link to="/saleitem-List" className="unset-anchor nav-link">SaleItemList</Link>
                         </li>
-                        <li className="nav-item active">
+                        {/* <li className="nav-item active">
                             <Link to="/new-sale" className="unset-anchor nav-link">Create Sale</Link>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
             </nav>
